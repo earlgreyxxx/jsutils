754f21a9feb7b466e5ecc839bde4888b7bf41822
Kenji Nakagawa <nakagawa@mars.dti.ne.jp>
2023-12-22 16:10:22 +0900

    fetch-utilsへキャッシュ機能を追加しました。

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -4,15 +4,46 @@
 *    - export functions fetchJson,fetchText,fetchBlob and fetchResponse
 *
 *****************************************************************************/ 
+import { isFunction } from "./type.js";
+import CachedFetch from "./cached-fetch.js";
 import ObjectToFormData from "./object-formdata.js";
 
 export const fetchJson = (url,params,fetchOptions) => fetchResponse(url,params,fetchOptions).then(response => response.json());
 export const fetchText = (url,params,fetchOptions) => fetchResponse(url,params,fetchOptions).then(response => response.text());
 export const fetchBlob = (url,params,fetchOptions) => fetchResponse(url,params,fetchOptions).then(response => response.blob());
 
+export const cachedJson = (url,params,fetchOptions) => cachedResponse(url,params,fetchOptions).then(response => response.json());
+export const cachedText = (url,params,fetchOptions) => cachedResponse(url,params,fetchOptions).then(response => response.text());
+export const cachedBlob = (url,params,fetchOptions) => cachedResponse(url,params,fetchOptions).then(response => response.blob());
+
 // non blocking fetch
 export function fetchResponse(url,params,fetchOptions = {})
 {
+  return __imp_fetchResponse(globalThis.fetch,url,params,fetchOptions)
+}
+
+// url must have prefix 'cache:{appname,expire time(second)}/'
+// ex. cachedResponse('cache:apps1,3600;http://hogehoge.com/api/get');
+export function cachedResponse(url,params,fetchOptions = {})
+{
+  const m = url.match(/^cache:(\w+),(\d+);(.+)/);
+  if(!m)
+    throw new Error('1st argument is valid pattern');
+
+  m.shift();
+  const [name,expire,url] = m;
+  if(expire <= 0)
+    throw new Error('expire must be higher than 0');
+
+  const cf = new CachedFetch(name,expire);
+  return __imp_fetchResponse(cf.fetch,url,params,fetchOptions)
+}
+
+function __imp_fetchResponse(funcFetch,url,params,fetchOptions = {})
+{
+  if(!isFunction(funcFetch))
+    throw new Error('1st argument not function');
+
   const options = params ? { method: 'POST',body: ObjectToFormData(params), ...fetchOptions } : { method: 'GET', ...fetchOptions };
   if(!options.body)
     options.method = 'GET';
@@ -35,5 +66,5 @@ export function fetchResponse(url,params,fetchOptions = {})
   const fail = e => {
     throw e;
   };
-  return fetch(url,options).then(done).catch(fail);
+  return funcFetch(url,options).then(done).catch(fail);
 }
