395370e9a816069bd4dc75624ae0353b30bb16ee
Kenji Nakagawa <nakagawa@mars.dti.ne.jp>
2024-11-06 16:53:45 +0900

    Dom.offメソッドの追加

--- utility/dom-utils.js
+++ utility/dom-utils.js
@@ -1,7 +1,7 @@
 /******************************************************************************
  *  DOM helper utilities
 ******************************************************************************/
-import { isString,isHtmlElement,isNodeList,isArrowFunction } from './type.js';
+import { isString,isHtmlElement,isNodeList,isFunction,isArrowFunction } from './type.js';
 
 export const createElement = str => new DOMParser().parseFromString(str,"text/html").body.firstElementChild;
 
@@ -37,7 +37,9 @@ export function hide(element)
   element.style.setProperty('display','none');
 }
 
-// alternative for jQuery.on(eventname,selector,handler)
+const _handlers = new Map;
+
+// alternative for jQuery.on(eventname,selector,handler) and .on(eventname,handler)
 export function on(element_or_selector,eventname,selector,handler)
 {
   const invoker = (_,event) => isArrowFunction(handler) ? handler(_,event) : handler.call(_,event);
@@ -48,9 +50,14 @@ export function on(element_or_selector,eventname,selector,handler)
       invoker(element,ev);
   }
 
+  const eventHandler = isFunction(selector) ? selector : listener;
+
   if(isHtmlElement(element_or_selector))
   {
-    element_or_selector.addEventListener(eventname,listener);
+    const element = element_or_selector;
+    _handlers.set(element,{ eventname, eventHandler });
+
+    element.addEventListener(eventname,eventHandler);
   }
   else
   {
@@ -62,6 +69,44 @@ export function on(element_or_selector,eventname,selector,handler)
     else
       throw new Error('argument type error');
 
-    root.forEach(el => el.addEventListener(eventname,listener));
+    root.forEach(el => {
+      el.addEventListener(eventname,eventHandler);
+      _handlers.set(el,{ eventname,eventHandler });
+    });
   }
+}
+
+// alternative for jQuery.off(eventname)
+export function off(element_or_selector,eventname)
+{
+  if(isHtmlElement(element_or_selector))
+  {
+    const element = element_or_selector;
+    const item = _handlers.get(element);
+    if(item && item.eventname === eventname)
+    {
+      element.removeEventListener(eventname, item.eventHandler);
+      _handlers.remove(element);
+    }
+  }
+  else
+  {
+    let root;
+    if(isNodeList(element_or_selector))
+      root = element_or_selector;
+    else if(isString(element_or_selector))
+      root = document.querySelectorAll(element_or_selector) ?? [];
+    else
+      throw new Error('argument type error');
+
+    root.forEach(el => {
+      const item = _handlers.get(el);
+      if(item && item.eventname === eventname)
+      {
+        el.removeEventListener(eventname,item.eventHandler);
+        _handlers.remove(el);
+      }
+    });
+  }
+  
 }
