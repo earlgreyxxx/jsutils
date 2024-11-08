9de93d1bc3fe0e5e88e13d69bc213060e0825c71
Kenji Nakagawa <nakagawa@mars.dti.ne.jp>
2024-11-08 16:57:00 +0900

    Dom.query,Dom.queriesメソッドの追加及びその他修正

--- utility/dom-utils.js
+++ utility/dom-utils.js
@@ -1,5 +1,17 @@
 /******************************************************************************
  *  DOM helper utilities
+ * 
+ *  exports:
+ *   createElement(string)
+ *   create(string)
+ *   empty(HTMLElement)
+ *   css(HTMLElement,object)
+ *   show(HTMLElement,string = 'block')
+ *   hide(HTMLElement)
+ *   on(HTMLElement|string,string,string,Function)
+ *   off(HTMLElement|string,string)
+ *   query(string,HTMLElement|Document = null)
+ *   queries(string,HTMLElement|Document = null)
 ******************************************************************************/
 import { isString,isHtmlElement,isNodeList,isFunction,isArrowFunction } from './type.js';
 
@@ -22,25 +34,47 @@ export function empty(element)
     element.removeChild(element.firstChild);
 }
 
-export function css(element,style)
-{
-  Object.entries(style).forEach(([ property,value ]) => element.style.setProperty(property.replace(/[A-Z]/g,m => ('-'+m.toLowerCase())),value,'important'));
-}
-
-export function show(element,value = 'block')
-{
-  element.style.setProperty('display',value);
-}
-
-export function hide(element)
-{
-  element.style.setProperty('display','none');
-}
+export const css = (element,style) => Object.entries(style).forEach(([ property,value ]) => element.style.setProperty(property.replace(/[A-Z]/g,m => ('-'+m.toLowerCase())),value,'important'));
+export const show = (element,value = 'block') => element.style.setProperty('display',value);
+export const hide = element => element.style.setProperty('display','none');
 
 // _handlers:map<myId:string,names:map<name:string,handlers:[]>>
 const _handlers = new Map;
+const _inserter = (el, nm, eh) => {
+  const myId = el.dataset.myId = Math.random().toString().slice(2);
+  if (!_handlers.has(myId))
+    _handlers.set(myId, new Map);
 
-// alternative for jQuery.on(eventname,selector,handler) and .on(eventname,handler)
+  const eventnames = _handlers.get(myId);
+  if (!eventnames.has(nm))
+    eventnames.set(nm, [])
+
+  const handlers = eventnames.get(nm);
+
+  handlers.push(eh);
+};
+const _remover = (el, nm) => {
+  const { myId } = el.dataset;
+  if (!myId)
+    return;
+
+  const eventnames = _handlers.get(myId);
+  if (!eventnames)
+    return;
+
+  const handlers = eventnames.get(nm) ?? [];
+  eventnames.delete(nm);
+
+  if(eventnames.size == 0)
+  {
+    _handlers.delete(myId);
+    delete el.dataset.myId;
+  }
+
+  return handlers;
+};
+
+// alternative for jQuery .on(eventname,selector,handler) and .on(eventname,handler)
 export function on(element_or_selector,eventname,selector,handler)
 {
   const invoker = (_,event) => isArrowFunction(handler) ? handler(_,event) : handler.call(_,event);
@@ -53,24 +87,10 @@ export function on(element_or_selector,eventname,selector,handler)
 
   const eventHandler = isFunction(selector) ? selector : listener;
 
-  const inserter = (el,nm,eh) => {
-    const myId = el.dataset.myId = Math.random().toString().slice(2);
-    if(!_handlers.has(myId))
-      _handlers.set(myId,new Map);
-
-    const eventnames = _handlers.get(myId);
-    if(!eventnames.has(nm))
-      eventnames.set(nm,[])
-
-    const handlers = eventnames.get(nm);
-
-    handlers.push(eh);
-  };
-
   if(isHtmlElement(element_or_selector))
   {
     const element = element_or_selector;
-    inserter(element,eventname,eventHandler);
+    _inserter(element,eventname,eventHandler);
     element.addEventListener(eventname,eventHandler);
   }
   else
@@ -84,34 +104,19 @@ export function on(element_or_selector,eventname,selector,handler)
       throw new Error('argument type error');
 
     root.forEach(el => {
-      inserter(el, eventname, eventHandler);
+      _inserter(el, eventname, eventHandler);
       el.addEventListener(eventname,eventHandler);
     });
   }
 }
 
-// alternative for jQuery.off(eventname)
+// alternative for jQuery .off(eventname)
 export function off(element_or_selector,eventname)
 {
-  const remover = (el,nm) => {
-    const { myId } = el.dataset;
-    if(!myId)
-      return;
-
-    const eventnames = _handlers.get(myId);
-    if(!eventnames)
-      return;
-
-    const handlers = eventnames.get(nm) ?? [];
-    eventnames.delete(nm);
-
-    return handlers;
-  };
-
   if(isHtmlElement(element_or_selector))
   {
     const element = element_or_selector;
-    remover(element,eventname).forEach(handler => element.removeEventListener(eventname, item.eventHandler));
+    _remover(element,eventname)?.forEach(handler => element.removeEventListener(eventname, item.eventHandler));
   }
   else
   {
@@ -123,8 +128,26 @@ export function off(element_or_selector,eventname)
     else
       throw new Error('argument type error');
 
-    root.forEach(el => {
-      remover(el, eventname).forEach(handler => el.removeEventListener(eventname, handler));
-    });
+    root.forEach(el => _remover(el, eventname)?.forEach(handler => el.removeEventListener(eventname, handler)));
   }
+}
+
+// shortcut for querySelector and querySelectorAll
+// selector:string, element:HTMLElement | HTMLDocument
+// This method returns HTMLElement or null
+export function query(selector,element)
+{
+  if(!(element instanceof Document) && !(element instanceof HTMLElement))
+    element = document;
+
+  return element.querySelector(selector);
+}
+
+// same as query, but return NodeList or empty NodeList
+export function queries(selector,element)
+{
+  if(!(element instanceof Document) && !(element instanceof HTMLElement))
+    element = document;
+
+  return element.querySelectorAll(selector);
 }
